3962551e7ab724d26d18948f48a1b56f4e001192
Rohith-Dasari <rohithdasariformal@gmail.com>
2026-02-02 00:40:32 +0530

    clean up

--- stack_sums/next_greater_element.py
+++ stack_sums/next_greater_element.py
@@ -14,5 +14,33 @@ def nextGreaterElement(nums1: List[int], nums2: List[int]) -> List[int]:
                         result[idx]=-1
                     break
         return result
+#part1
+#MONOTONIC STACK
+def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    stack = []
+    next_greater = {}
+    for i in range(len(nums2) - 1, -1, -1):
+        num = nums2[i]
+        while stack and stack[-1] <= num:
+            stack.pop()
+        next_greater[num] = stack[-1] if stack else -1
+        stack.append(num)
+    return [next_greater[num] for num in nums1]
 
+
+#PART 2
 #monotonic stack approach
+def nextGreaterElements(nums: List[int]) -> List[int]:
+    length=len(nums)
+    nge_stack=[]
+    result=[-1]*length
+    for idx in range(2*length-1,-1,-1):
+        element = nums[idx % length]
+        while nge_stack and nge_stack[-1]<=element:
+            nge_stack.pop()
+        if idx<length:
+            result[idx]=nge_stack[-1] if nge_stack else -1
+        nge_stack.append(element)
+
+    return result
+print(nextGreaterElements([1,2,3,4,3]))
